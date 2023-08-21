a0ffef9dc78db9fcdeb42f58c57dcbc6c4589aca
Lola Castiilo <larrycastillo2009@gmail.com>
2023-08-21 14:01:26 -0500

    list js added

---
+++ src/components/List.js
@@ -0,0 +1,10 @@
+function List(){
+    return <h2>Characters</h2>;
+}
+
+function Character(){
+    return <h3>Character</h3>;
+}
+
+// export default List;
+export default Character;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import ReactDOM from 'react-dom/client';
+import Character from './components/List'
 
 function App(){
-    return <h1>Rick and Morty</h1>;
+    return (<div>
+        <h1>Rick and Morty</h1>
+            <h2>Characters</h2>
+            <Character />
+            <Character />
+</div>
+    );
 }
 
 const container = document.getElementById('app');
